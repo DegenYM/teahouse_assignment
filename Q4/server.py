c6a83155c364bdf49a69a078e5a9799eb4891832
DegenYM <117725445+DegenYM@users.noreply.github.com>
2022-11-09 09:29:51 +0800

    Update server.py

--- Q4/server.py
+++ Q4/server.py
@@ -4,6 +4,15 @@ from web3 import Web3
 app = Flask(__name__)
 w3 = Web3(Web3.HTTPProvider("https://mainnet.infura.io/v3/ee96256c1e6643e39b8e6cd27d471e73"))
 
+def abort_msg(e):
+    error_class = e.__class__.__name__ # error class
+    detail = e.args[0]
+
+    # generate the error message
+    errMsg = "[{}] {}.".format(error_class, detail)
+    # return 500 code
+    abort(500, errMsg)
+    
 @app.route('/')
 def hello():
     return 'Hello TeaHouse'
@@ -19,20 +28,23 @@ def get_balance():
     elif request.method == 'POST':
         token_address = request.form['token_address']
         query_address = request.form['query_address']
+    try:
+        token = w3.eth.contract(address=token_address, abi=contract_abi)
+        token_name = token.functions.symbol().call()
+        token_balance = token.functions.balanceOf(query_address).call()
+        decimals = token.functions.decimals().call()
 
-    token = w3.eth.contract(address=token_address, abi=contract_abi)
-    token_name = token.functions.symbol().call()
-    token_balance = token.functions.balanceOf(query_address).call()
-    decimals = token.functions.decimals().call()
+        output = {'token_name': token_name, 
+                'token_address': token_address, 
+                'query_address': query_address,
+                'query_raw_balance': token_balance, 
+                'query_balance': token_balance/(10**decimals)}
 
-    output = {'token_name': token_name, 
-            'token_address': token_address, 
-            'query_address': query_address,
-            'query_raw_balance': token_balance, 
-            'query_balance': token_balance/(10**decimals)}
+        return jsonify(output)
 
-    return jsonify(output)
-
+       except Exception as e:
+            abort_msg(e)
+            
 if __name__ == '__main__':
     init_host = '127.0.0.1'
     app.run(host=init_host,port=22345)
